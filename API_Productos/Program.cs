da117a9b240683f86bbfbfe3c8012022cc70c371
Cristofer Span <cspan@ext.sancorseguros.com>
2025-08-12 08:02:32 -0300

    activo swagger para todo ambiente

--- API_Productos/Program.cs
+++ API_Productos/Program.cs
@@ -21,11 +21,10 @@ namespace CristoferSpan_APO_Productos
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+            
 
             app.UseHttpsRedirection();
 
